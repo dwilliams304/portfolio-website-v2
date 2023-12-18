949d20fe4da7c99e417aa185856416a0f2d614e4
dwilliams304 <d.williams0434@gmail.com>
2023-12-18 11:05:03 -0700

    Styling

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable */
+import "../../styles/section-styles/contact.css"
+
 export default function Contact(){
     return(
         <section className="contact" id="contact">
-            <h2>Contact</h2>
+            <h2>Let's <span className="highlight">Connect!</span></h2>
             <form>
-                <input type="text" placeholder="Full Name" />
-                <input type="email" placeholder="Email Address" />
-                <input type="text" placeholder="Phone #" />
-                <input type="text" placeholder="Subject" />
-                <input type="text" placeholder="Additional Details" />
+                <input type="text" name="form_name" placeholder="Full Name" />
+                <input type="email" name="form_email" placeholder="Email Address" />
+                <input type="tel" name="form_phone" placeholder="Phone # - (123) 456-7890" />
+                <input type="text" name="form_subject" placeholder="Subject" />
+                <textarea placeholder="Additional details"></textarea>
                 <button type="submit">Send!</button>
             </form>
         </section>
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,16 +1,35 @@
+import "../../styles/section-styles/projects.css";
+
 export default function Projects(){
     return(
         <section className="projects" id="projects">
             <h2>My <span className="highlight">Projects</span></h2>
-            <div className="project">
-                <h4>Project Name</h4>
-                <img src="" alt="Project thumb"/>
-                <div>
-                    <span>Tech Used</span>
+            <div className="scrolling-container">
+                <div className="project">
+                    <h4>Neon Rain</h4>
+                    <img src="" alt="Project thumb"/>
+                    <div>
+                        <p>Tech Used</p>
+                        <span>JS</span>
+                        <span>Unity</span>
+                    </div>
+                    <p>Neon Rain is a 2D top-down shooter game made within the Unity Engine.</p>
+                    <button>GitHub</button>
+                    <button>Demo</button>
                 </div>
-                <p>Project details!</p>
-                <button>GitHub</button>
-                <button>Demo</button>
+
+                <div className="project">
+                    <h4>Neon Rain</h4>
+                    <img src="" alt="Project thumb"/>
+                    <div>
+                        <p>Tech Used</p>
+                        <span>JS</span>
+                        <span>Unity</span>
+                    </div>
+                    <p>Neon Rain is a 2D top-down shooter game made within the Unity Engine.</p>
+                    <button>GitHub</button>
+                    <button>Demo</button>
+                </div>
             </div>
         </section>
     )
