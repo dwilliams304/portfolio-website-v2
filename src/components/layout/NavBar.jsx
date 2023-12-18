d1ec05b36a8905c591ce3fefafa3b07d1f7bb982
dwilliams304 <d.williams0434@gmail.com>
2023-12-18 09:17:07 -0700

    Initial commit

---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,17 @@
+
+
+function NavBar(){
+    return(
+        <nav>
+            <h1>daviswilliams.dev</h1>
+
+
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+        </nav>
+    )
+}
+
+export default NavBar;
