967ec729f0392f95880053a4e60076b386fb38eb
dwilliams304 <d.williams0434@gmail.com>
2024-02-03 09:41:22 -0700

    Update deps, mobile nav responsiveness wip

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { headers } from "../../data/RandomHeaders";
 
 export default function NavBar(){
     const [headerText, setHeaderText] = useState('daviswilliams');
+    const navRef = useRef();
+
+    const showMobileNav = () => {
+        navRef.current.classList.toggle('responsive-nav');
+    }
 
     const changeText = () => {
         var r = Math.floor(Math.random() * headers.length);
@@ -10,14 +15,24 @@ export default function NavBar(){
     }
 
     return(
-        <nav>
-            <h1 onClick={changeText}>{headerText}.<span className="highlight">dev</span></h1>
+        <header>
+            <nav ref={navRef}>
+                <h1 onClick={changeText}>{headerText}.<span className="highlight">dev</span></h1>
 
 
-            <a className="fancy-underline" href="#home">Home</a>
-            <a className="fancy-underline" href="#about">About</a>
-            <a className="fancy-underline" href="#projects">Projects</a>
-            <a className="fancy-underline" href="#contact">Contact</a>
-        </nav>
+                <a className="fancy-underline" href="#home">Home</a>
+                <a className="fancy-underline" href="#about">About</a>
+                <a className="fancy-underline" href="#projects">Projects</a>
+                <a className="fancy-underline" href="#contact">Contact</a>
+
+
+                <button className="nav-btn close-nav-btn">
+
+                </button>
+            </nav>
+            <button className="nav-btn">
+
+            </button>
+        </header>
     )
 }
