c46a56e4b3adb3cd11635e7c11348f1507f4eb12
dwilliams304 <d.williams0434@gmail.com>
2024-04-30 18:57:21 -0700

    switch to multi carousel for projects (finally)

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,26 @@
 import "../../styles/section-styles/projects.css";
-import CardCarousel from "../utility/CardCarousel";
 import { useState, useEffect } from "react";
 import { supabase } from "../../data/supabaseClient";
+
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+
+import Card from '../common/Card';
+
+const responsive = {
+    desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 3,
+    },
+        tablet: {
+        breakpoint: { max: 1024, min: 700 },
+        items: 2,
+    },
+        mobile: {
+        breakpoint: { max: 700, min: 0 },
+        items: 1,
+    }
+}
 
 export default function Projects(){
     const [cardsData, setCardsData] = useState([]);
@@ -27,7 +46,19 @@ export default function Projects(){
         <section className="projects" id="projects">
             <h2>My <span className="highlight">Projects</span></h2>
             <h4>Scroll to see more --&gt;</h4>
-                <CardCarousel cardsData={cardsData} />
+            <Carousel 
+                responsive={responsive}
+                infinite={true}
+                showDots={true}
+                containerClass="carousel-container"
+                dotListClass="carousel-dots"
+            >
+                {
+                    cardsData.map(card => {
+                        return <Card data={card} key={card.project_name}/>
+                    })
+                }
+            </Carousel>
         </section>
     )
 }
